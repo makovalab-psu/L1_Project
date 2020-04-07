3b8f8387b7a79ca3e877301f9fefa323239a1f71
Bruce <cdbruce@github.com>
2020-04-07 15:54:20 -0400

    Updated RS distance plot

--- Analysis/Additional_analysis/Restriction_sites/restriction_sites_plot.R
+++ Analysis/Additional_analysis/Restriction_sites/restriction_sites_plot.R
@@ -12,19 +12,13 @@ setwd("~/Google_Drive/L1_Project/Analysis/Additional_analysis/Restriction_sites/
 load('RS_Distance.RData')
 
 #Plot the distribution
-#Violin plot of coverage distribution 
-#install.packages("vioplot")
-library(vioplot)
-vioplot(log(distance+0.0001), names=c("Log Distance Distribution Between MSPI and TaqI Sites"), ylab="Log Distance")
-abline(h=log(150+0.0001), col="green")
-#ylim = c(0,0.02))
-
 #Histogram of coverage distribution 
 #hist(distance, xlim=c(0,10000))
-hist(log(distance+0.0001), main = "Log Distance Distribution Between MSPI and TaqI Sites", xlab="Log Distance")
+pdf("Distance_distribution_MSPI_TaqI.pdf")
+hist(log(distance+0.0001), main = "Log Distance Distribution Between MspI and TaqI Sites", xlab="Log Distance")
 #add cutoff of 150 bp (average read length)
 abline(v=log(150+0.0001), col="green")
-
+dev.off()
 
 #Calculate percentage of distance values under 150 bp (average read length)
 length(distance)
